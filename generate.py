6ff4bb608f58a2219bd7526058bfb9d9bd07fb98
raajtilaksarma <raajtilaksarma@gmail.com>
2019-12-07 13:21:18 +0530

    y axis centering removed

--- generate.py
+++ generate.py
@@ -80,9 +80,8 @@ class Create:
                 
                 # center allign text 
                 w, h = draw.textsize(data[j][i])
-
                 #draw on image
-                draw.text([self.values[j][0]-(w/2),self.values[j][1]-(h/2)], data[j][i],font=fontName, fill=self.font['color'])
+                draw.text([self.values[j][0]-(w/2),self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
             
             #save image separately
             #also save in database --> verified folder if verification is checked 
