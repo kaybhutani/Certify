6d2259b77e9654fa6ca65b9eec63760857f39286
Rishabh Jain <51448962+rishabh214@users.noreply.github.com>
2020-01-02 01:50:06 +0530

    Add files via upload

--- generate.py
+++ generate.py
@@ -115,10 +115,15 @@ class Create:
                 #get font for that element
                 fontName = ImageFont.truetype(self.font['name'], fontSize)
 
-                #draw on image
+                #center align
                 h,w=draw.textsize(data[j][i])
                 if (j=='College' or j=='Position'):
-                    draw.text([self.values[j][0]+30*3-(h/2)*3,self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
+                    draw.text([self.values[j][0]+31.5*3-(h/2)*3,self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
+                elif(j=='Event'):
+                    if (h>48):
+                        draw.text([self.values[j][0]+23*6-(h/2)*6,self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
+                    else:
+                        draw.text([self.values[j][0]+23*3-(h/2)*3,self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
                 else:
                     draw.text([self.values[j][0]+24*6-(h/2)*6,self.values[j][1]], data[j][i],font=fontName, fill=self.font['color'])
 
